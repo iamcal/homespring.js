9c41d450ac6a03222930fb3f4e77d484965100ce
Cal Henderson <cal@iamcal.com>
2017-01-07 21:59:44 -0600

    implemented tests for young salmon versus shallows fixed shallows nodes to only affect mature salmon

--- test/04_shallows.js
+++ test/04_shallows.js
@@ -34,14 +34,28 @@ describe("keyword: shallows", function(){
 
 	it("does not slow young upstream salmon", function(){
 
-		// TODO
-		expect(true).toBe(false);
+		var p = new HS.Program('foo shallows bar');
+		var s = new HS.Salmon(p, '', HS.const.YOUNG, HS.const.UPSTREAM);
+
+		p.findFirstNode('foo').fishEnters(s);
+
+		p.tick();
+		expect(s.findNode().name).toBe('shallows');
+		p.tick();
+		expect(s.findNode().name).toBe('bar');
 	});
 
 	it("does not slow young downstream salmon", function(){
 
-		// TODO
-		expect(true).toBe(false);
+		var p = new HS.Program('foo shallows bar');
+		var s = new HS.Salmon(p, '', HS.const.YOUNG, HS.const.DOWNSTREAM);
+
+		p.findFirstNode('bar').fishEnters(s);
+
+		p.tick();
+		expect(s.findNode().name).toBe('shallows');
+		p.tick();
+		expect(s.findNode().name).toBe('foo');
 	});
 });
 

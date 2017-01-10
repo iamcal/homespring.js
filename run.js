72380dd5a808d36358240665f8afbde809d6ff9f
Cal Henderson <cal@iamcal.com>
2017-01-09 23:29:13 -0600

    a simple node run wrapper for testing demo scripts

---
+++ run.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+var HS = require('./lib/homespring.js');
+
+var path = process.argv[2];
+
+fs.readFile(path, 'utf8', function(err, data){
+	if (err){
+		console.log('Unable to read source file: '+err);
+	}else{
+		var p = new HS.Program(data, true);
+		p.run();
+	}
+});

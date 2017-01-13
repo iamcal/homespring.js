b2d68c0a7c58e472c917824b5a4e98cda975d30d
Cal Henderson <cal@iamcal.com>
2017-01-13 00:14:03 -0600

    need real flags here at some point

--- run.js
+++ run.js
@@ -5,19 +5,21 @@ var HS = require('./lib/homespring.js');
 
 var path = process.argv[2];
 
+var debug = false;
+
 fs.readFile(path, 'utf8', function(err, data){
 	if (err){
 		console.log('Unable to read source file: '+err);
 	}else{
 		var p = new HS.Program(data, false);
-		p.dumpState()
+		if (debug) p.dumpState()
 
 		var max = 20;
 		var i = 0;
 
 		while (i<max && !p.endThisTick){
 			p.tick();
-			p.dumpState();
+			if (debug) p.dumpState();
 			i++;
 		}
 

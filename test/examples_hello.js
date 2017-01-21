642516c55ef06c6c330cab3dad1d595afcdc8626
Cal Henderson <cal@iamcal.com>
2017-01-20 22:15:59 -0600

    also test hello-[2-4].hs

--- test/examples_hello.js
+++ test/examples_hello.js
@@ -13,4 +13,43 @@ describe("examples: hello*", function(){
 		p.test();
 	});
 
+	it("runs hello-2.hs", function(){
+
+		var p = test_example('hello-2.hs', 20);
+
+		p.onTerminate = function(){
+
+			expect(p.tickNum).toBe(10);
+			expect(p.output).toEqual(['Hello World!\n']);
+		};
+
+		p.test();
+	});
+
+	it("runs hello-3.hs", function(){
+
+		var p = test_example('hello-3.hs', 20);
+
+		p.onTerminate = function(){
+
+			expect(p.tickNum).toBe(16);
+			expect(p.output).toEqual(['Hello, World!\n']);
+		};
+
+		p.test();
+	});
+
+	it("runs hello-4.hs", function(){
+
+		var p = test_example('hello-4.hs', 10);
+
+		p.onTerminate = function(){
+
+			expect(p.tickNum).toBe(9);
+			expect(p.output).toEqual(['hell', 'o ', 'world\n']);
+		};
+
+		p.test();
+	});
+
 });

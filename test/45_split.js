89bba1353aecd861823f7deee61c3eb638d13c9d
Cal Henderson <cal@iamcal.com>
2017-01-21 23:47:48 -0600

    include test programs for the ordering of split nodes

--- test/45_split.js
+++ test/45_split.js
@@ -21,5 +21,32 @@ describe("keyword: split", function(){
 		expect(n.salmon[2].name).toBe('c');
 	});
 
+	it("test program - split one", function(){
+
+		var p = new HS.Program('foo split bear hatchery powers abc     universe marshy marshy marshy marshy snowmelt');
+
+		var output = [];
+		p.onOutput = function(str){ output.push(str); }
+
+		for (var i=0; i<20; i++) p.tick();
+
+		expect(p.terminated).toBe(true);
+		expect(p.tickNum).toBe(10);
+		expect(output).toEqual(['c', 'b', 'a']);
+	});
+
+	it("test program - split two", function(){
+
+		var p = new HS.Program('foo split bear hatchery powers abc     bear hatchery powers def     universe marshy marshy marshy marshy snowmelt');
+
+		var output = [];
+		p.onOutput = function(str){ output.push(str); }
+
+		for (var i=0; i<20; i++) p.tick();
+
+		expect(p.terminated).toBe(true);
+		expect(p.tickNum).toBe(10);
+		expect(output).toEqual(['c', 'b', 'a', 'f', 'e', 'd']);
+	});
 });
 

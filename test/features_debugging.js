19179326b79b671bd377992cea13b4f43e00dbc5
Cal Henderson <cal@iamcal.com>
2017-05-09 15:55:59 -0500

    dumpFish is not used ; added getSalmon instead, for easier debugging. added tests for salmon debugging

--- test/features_debugging.js
+++ test/features_debugging.js
@@ -22,5 +22,22 @@ describe("feature: debugging", function(){
 		expect(p2.nodes[1].toString()).toBe('force field:p:BW:BS');
 		expect(p2.nodes[2].toString()).toBe('powers:P:p');
 	});
+
+	it("salmon return good state information", function(){
+
+		var p = new HS.Program("Universe bear hatchery Hello. World!.\n Powers   marshy marshy snowmelt");
+		p.runSync(20);
+
+		var s = p.getSalmon();
+
+		expect(s[0][0]+'/'+s[0][1]).toBe("Universe:destroyed:p:s:w/Hello World!\\n:young:down");
+		expect(s[1][0]+'/'+s[1][1]).toBe("bear:p:s:w/Hello World!\\n:young:down");
+		expect(s[2][0]+'/'+s[2][1]).toBe("hatchery:p:w/homeless:young:up");
+		expect(s[3][0]+'/'+s[3][1]).toBe("hatchery:p:w/homeless:mature:down");
+		expect(s[4][0]+'/'+s[4][1]).toBe("hatchery:p:w/Hello World!\\n:young:down");
+		expect(s[5][0]+'/'+s[5][1]).toBe("Hello World!\\n:w/Hello World!\\n:young:down");
+		expect(s[6][0]+'/'+s[6][1]).toBe("Hello World!\\n:w/homeless:young:up");
+		expect(s[7][0]+'/'+s[7][1]).toBe("Hello World!\\n:w/homeless:mature:down");
+	});
 });
 
